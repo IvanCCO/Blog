38848d79858969c30e9b8af7ce1645255dd1b46f
IvanCCO <ivanmedeiros0903@outlook.com>
2023-12-23 19:07:18 -0300

    Its finally working - i hope so - markdown defeat

---
+++ client/src/components/Markdown/MarkdownCode.tsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react";
+import Markdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { prism } from "react-syntax-highlighter/dist/esm/styles/prism";
+import EX from "../../assets/Markdown/ex.md";
+import remarkGfm from "remark-gfm";
+
+export default function MarkdownCode() {
+  const [post, setPost] = useState("");
+
+  useEffect(() => {
+    fetch(EX)
+      .then((res) => res.text())
+      .then((md) => {
+        setPost(md);
+      });
+  }),
+    [];
+
+  console.log(post);
+
+  return (
+    <Markdown
+      children={post}
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code(props) {
+          const { children, className, node, ...rest } = props;
+          const match = /language-(\w+)/.exec(className || "");
+          return match ? (
+            <SyntaxHighlighter
+              PreTag="div"
+              children={String(children).replace(/\n$/, "")}
+              language={match[1]}
+              style={prism}
+            />
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
+        h1(props) {
+          return (
+            <h1 className="text-3xl font-semibold my-9" {...props}>
+              {props.children}
+            </h1>
+          );
+        },
+        h2(props) {
+          return (
+            <h1 className="text-xl font-semibold my-7" {...props}>
+              {props.children}
+            </h1>
+          );
+        },
+        h3(props) {
+          return (
+            <h1 className="text-lg font-semibold my-5" {...props}>
+              {props.children}
+            </h1>
+          );
+        },
+        h4(props) {
+          return (
+            <h1 className="text-base font-semibold my-5" {...props}>
+              {props.children}
+            </h1>
+          );
+        },
+        ul(props) {
+          return (
+            <ul className="list-disc ml-4" {...props}>
+              {props.children}
+            </ul>
+          );
+        },
+        li(props) {
+          return (
+            <li className="my-6" {...props}>
+              {props.children}
+            </li>
+          );
+        },
+      }}
+    />
+  );
+}
--- client/src/pages/Post/Post.tsx
+++ client/src/pages/Post/Post.tsx
@@ -1,65 +1,15 @@
 import { PageType } from "../../data/constants";
 import { Header } from "../../components/Header";
-import MarkdownHtml from "../../components/Markdown/MarkdownHtml";
+import MarkdownCode from "../../components/Markdown/MarkdownCode";
 
 export function Post() {
-  const markdown = `# Introdução ao Kubernetes: Orquestração de Contêineres
-
-O Kubernetes, frequentemente abreviado como K8s, é uma poderosa plataforma de orquestração de contêineres de código aberto que automatiza a implantação, o dimensionamento e a gestão de aplicativos em contêineres. Desenvolvido pelo Google e agora mantido pela Cloud Native Computing Foundation (CNCF), o Kubernetes se tornou uma peça fundamental na construção e operação de aplicações distribuídas em escala.
-
-## O que são Contêineres?
-
-Antes de mergulharmos no Kubernetes, vamos entender o que são contêineres. Os contêineres são unidades leves e portáteis que encapsulam o código, suas dependências e configurações. Eles garantem que uma aplicação seja executada de maneira consistente em diferentes ambientes.
-
-## Por que Kubernetes?
-
-À medida que as aplicações crescem em complexidade, gerenciar contêineres manualmente se torna desafiador. Aqui é onde o Kubernetes brilha, oferecendo:
-
-- **Orquestração Avançada:** O Kubernetes gerencia automaticamente a distribuição e escalabilidade de contêineres, garantindo que as aplicações estejam sempre disponíveis e funcionando corretamente.
-
-- **Escalabilidade Horizontal:** O Kubernetes facilita a escala de aplicações horizontalmente, adicionando ou removendo contêineres conforme a demanda, mantendo assim o desempenho e a confiabilidade.
-
-- **Auto-Recuperação:** Em caso de falhas, o Kubernetes detecta e substitui automaticamente os contêineres com problemas, garantindo uma alta disponibilidade contínua.
-
-## Conceitos Chave do Kubernetes
-
-### Pods
-
-Os Pods são a menor unidade no Kubernetes e podem conter um ou mais contêineres. Eles compartilham um namespace de rede e armazenamento, facilitando a comunicação e a colaboração entre contêineres.
-
-### Services
-
-Os Serviços Kubernetes são usados para expor aplicações e seus componentes aos outros Pods. Eles fornecem uma abstração consistente para a comunicação entre Pods, independentemente de sua localização na rede.
-
-### Deployment
-
-Os Deployments gerenciam a implantação de aplicações e permitem atualizações sem tempo de inatividade. Eles garantem que o número desejado de réplicas esteja sempre em execução.
-
-## Exemplo Prático
-
-Vamos criar um Deployment simples para ilustrar o poder do Kubernetes:
-
-~~~html
-<>
-      <Header type={PageType.DEFAULT} />
-      <main className="h-screen py-32 px-default-width space-y-6">
-        <div className="border-2 border-purple-800 flex-row justify-between">
-          <div>
-            <MarkdownHtml text={markdown} />
-          </div>
-        </div>
-      </main>
-    </>
-~~~
-`;
-
   return (
     <>
       <Header type={PageType.DEFAULT} />
       <main className="h-screen py-32 px-default-width space-y-6">
         <div className="border-2 border-purple-800 flex-row justify-between">
           <div>
-            <MarkdownHtml text={markdown} />
+            <MarkdownCode />
           </div>
         </div>
       </main>
