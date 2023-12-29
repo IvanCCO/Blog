d0455b0b50f7294f1246e7e041df3702a8ce999c
IvanCCO <ivanmedeiros0903@outlook.com>
2023-12-29 19:14:07 -0300

    Not found page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,33 +1,66 @@
-import { Button, Center, Image, Text, VStack } from "@chakra-ui/react";
+import { QuestionOutlineIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Center,
+  HStack,
+  Image,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { Header } from "../../components/Header";
 import { PageType } from "../../data/constants";
 
 export function NotFound() {
   return (
     <main className="main sm:px-28 md:px-44 lg:px-96">
-      <Header type={PageType.DEFAULT}/>
+      <Header type={PageType.DEFAULT} />
       <Center>
         <Image
           src="https://th.bing.com/th/id/OIG.I7Xvxuab3JL68Nrm9VH_?w=1024&h=1024&rs=1&pid=ImgDetMain"
-          boxSize={["200px","300px" ]}
+          boxSize={["200px", "300px"]}
         />
       </Center>
 
       <VStack spacing={10}>
         <div>
-          <Text fontSize={["3xl", "4xl"]} fontWeight={"semibold"}>Hmmm, we couldn't find that page.</Text>
+          <Text fontSize={["3xl", "4xl"]} fontWeight={"semibold"}>
+            Hmmm, we couldn't find that page.
+          </Text>
 
           <Text fontSize={"lg"} color={"#727272"}>
             Go to our home page, that definitely does exists.
             <br></br>
-            If you think thats
-            a bug,{" "}
+            If you think thats a bug,{" "}
             <a href="#" className="link-color text-purple-800 font-semibold">
               contact me
             </a>
           </Text>
         </div>
-        <Button colorScheme="teal" variant={"solid"} size={["md", "lg"]}>Back to Home</Button>
+        <Button colorScheme="teal" variant={"solid"} size={["md", "lg"]}>
+          Back to Home
+        </Button>
+
+        <Popover isLazy>
+          <PopoverTrigger>
+            <HStack cursor={"pointer"}>
+              <QuestionOutlineIcon color={"#727272"} />
+              <Text fontSize={"xs"} color={"#727272"}>
+                Why a horse?
+              </Text>
+            </HStack>
+          </PopoverTrigger>
+          <PopoverContent>
+            <PopoverArrow />
+            <PopoverBody>
+              That's simple, my babys dream is to work with horse genetics
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
       </VStack>
     </main>
   );
