bc51e49900a851fdda4735b98988b25b0d4200fc
IvanCCO <ivanmedeiros0903@outlook.com>
2023-12-29 22:42:39 -0300

    I like the result but shit code again

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,8 +1,8 @@
 import {
-  AspectRatio,
   Card,
   CardBody,
   CardFooter,
+  Center,
   Heading,
   Image,
   Stack,
@@ -13,45 +13,53 @@ import { useNavigate } from "react-router-dom";
 export function MainCard() {
   const navigate = useNavigate();
 
-  return (
-    <>
       {/* TODO: Arrumar esse cara para ficar dinâmico */}
+  return (
+
+    <Center>
       <Card
-        maxW={["md", "lg"]}
+        direction={{ base: "column", md: "row" }}
         onClick={() => navigate("/post")}
         cursor={"pointer"}
         boxShadow={"lg"}
       >
-        <AspectRatio ratio={16 / 9}>
-          <Image
-            src="https://th.bing.com/th/id/OIG.pM5yvYt8jXgKE4HyVvUx?pid=ImgGn"
-            alt="A big octopus managing containers"
-            borderTopRadius="lg"
-            objectFit="cover"
-          />
-        </AspectRatio>
-        <CardBody>
-          <Stack spacing="3">
-            <Heading size="lg">Orquestando conteiners com Kubernets</Heading>
+        {/* <AspectRatio ratio={16 / 9}> */}
+        <Image
+          src="https://th.bing.com/th/id/OIG.pM5yvYt8jXgKE4HyVvUx?pid=ImgGn"
+          alt="A big octopus managing containers"
+          borderTopRadius={{ base: "lg"}}
+          borderLeftRadius="lg"
+          objectFit="cover"
+          aspectRatio={16/9}
+          maxW={{ md: "150px" }}
+        />
+        {/* </AspectRatio> */}
+        <Stack>
+          <CardBody>
+            <Heading size={["lg", "md"]} mb={3}>Orquestando conteiners com Kubernets</Heading>
             <Text
               bgGradient="linear(to-b, #1a1a1a 0%, rgba(118, 111, 154, 0.08) 100%)"
               backgroundClip="text"
+              maxW={"70%"}
             >
+              Que o Kubernetes está ficando o tal tal tal todo mundo sabe mas
               Que o Kubernetes está ficando o tal tal tal todo mundo sabe mas
               agora como de fato orquestrar conteiners com ele e fazer a
               diferenca é algo que pou...
             </Text>
-          </Stack>
-        </CardBody>
-        <CardFooter justify="space-between">
-          <Text fontSize="sm" color="gray.500">
-            4 min read
-          </Text>
-          <Text fontSize="sm" color="gray.500">
-            writed by @ivan_miranda
-          </Text>
-        </CardFooter>
+          </CardBody>
+          <CardFooter justify="space-between">
+            <Text fontSize="sm" color="gray.500">
+              4 min read
+            </Text>
+            <Text fontSize="sm" color="gray.500">
+              writed by @ivan_miranda
+            </Text>
+          </CardFooter>
+        </Stack>
       </Card>
-    </>
+
+
+    </Center>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Button, Text } from "@chakra-ui/react";
+import { Button, Text, Center } from "@chakra-ui/react";
 import { Header } from "../../components/Header";
 import { MainCard } from "../../components/MainCard";
 import { Pagination } from "../../components/Pagination";
@@ -15,7 +15,7 @@ export function Home() {
     <>
       <Header type={PageType.US} />
 
-      <main className="main space-y-8 grid place-items-center">
+      <main className="main space-y-8 grid place-items-center sm:px-28 md:px-44 lg:px-72">
         <div className="space-y-2">
           <Text fontSize={"3xl"} fontWeight={"medium"}>
             News
