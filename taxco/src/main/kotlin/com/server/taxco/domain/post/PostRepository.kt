34e8d268e7b20a9f46b4139606cc8de5a4183f25
IvanCCO <ivanmedeiros0903@outlook.com>
2024-01-18 22:05:52 -0300

    Adding some documentation

--- taxco/src/main/kotlin/com/server/taxco/domain/post/PostRepository.kt
+++ taxco/src/main/kotlin/com/server/taxco/domain/post/PostRepository.kt
@@ -6,13 +6,26 @@ import org.springframework.data.domain.Pageable
 
 // TODO: Pensar em alguma solução melhor para fazer isso aqui sem importar o pageable direto no domain
 
-/***
- * @repository for
- */
+/** responsible for make the contract of post database operations */
 interface PostRepository {
-
+    /**
+     * search for a specific Post domain object
+     * @param postId the post id that's going to be looking for
+     **/
     fun findById(postId: PostId): Post?
+    /**
+     *save a Post object into the database
+     * @param post the post object that's going to be saved
+     **/
     fun save(post: Post)
+    /**
+     * fetch all the posts per page
+     * @param pageable the object to decide the size and which page return
+     **/
     fun findAll(pageable: Pageable): Page<PostDocument>
+    /**
+     * find a post by post title
+     * @param name name of the post that's going to be search
+     **/
     fun findByTitle(name : String) : Post?
 }
