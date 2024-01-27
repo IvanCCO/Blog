8c7f14ed8641c1fc062820f77abcb865332dd7ae
IvanCCO <ivanmedeiros0903@outlook.com>
2024-01-26 22:59:00 -0300

    header auth

--- client/src/http/api.ts
+++ client/src/http/api.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const http = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
+  headers: {
+    "x-api-key": process.env.REACT_APP_API_SECRET,
+  },
 });
 
 export default http;
