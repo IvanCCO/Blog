5eebd3dd61f2011e97cf063bc9f00b5dd72ee6f8
IvanCCO <ivan.miranda@sptech.school>
2024-08-23 18:45:42 -0300

    add analytics to taxco

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 import { Route, Routes } from "react-router-dom";
 import { Post } from "../src/pages/Post/Post";
 import "./App.css";
@@ -10,16 +11,19 @@ import Places from "./pages/Places/Places";
 
 function App() {
   return (
-    <ChakraProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about/me" element={<He />} />
-        <Route path="/about/taxco" element={<About />} />
-        <Route path="/article/:articleId" element={<Post />} />
-        <Route path="/world" element={<Places />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </ChakraProvider>
+    <>
+      <ChakraProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about/me" element={<He />} />
+          <Route path="/about/taxco" element={<About />} />
+          <Route path="/article/:articleId" element={<Post />} />
+          <Route path="/world" element={<Places />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ChakraProvider>
+      <Analytics />
+    </>
   );
 }
 
