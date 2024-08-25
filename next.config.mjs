6513a1ab2d8d3d1157458daa5002c2697fb8e9f3
IvanCCO <ivan.miranda@sptech.school>
2024-08-25 01:19:20 -0300

    ee

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    allowMiddlewareResponseBody: true,
-  },
-  webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.pdf$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]',
-          },
-        },
-        {
-          loader: 'pdf-loader',
-        },
-      ],
-    });
-
-    return config;
+    allowMiddlewareResponseBody : true,
   },
 };
 
