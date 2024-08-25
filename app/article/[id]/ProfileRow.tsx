d688aa399d9ddf0aa3bd96c3ca57f098438f9945
IvanCCO <ivan.miranda@sptech.school>
2024-08-25 01:36:32 -0300

    Praticamente tudo pronto

--- app/article/[id]/ProfileRow.tsx
+++ app/article/[id]/ProfileRow.tsx
@@ -1,4 +1,6 @@
+import { formatDateWithYear } from "@/app/_lib/formatDate";
 import { Avatar, Box, Flex } from "@chakra-ui/react";
+import PIC from "../../../public/he-pic.jpg"
 
 export function ProfileRow({
   data,
@@ -16,11 +18,11 @@ export function ProfileRow({
       py={3}
       color={"white"}
     >
-      <Avatar name="Ivan Miranda" src={"c"} size={{ base: "md", md: "lg" }} />
+      <Avatar name="Ivan Miranda" src={PIC.src} size={{ base: "md", md: "lg" }} />
       <Box>
         <h1 className="text-sm sm:text-base lg:text-lg">Ivan Miranda</h1>
         <p className="text-xs text-neutral-300 sm:text-base">
-          Posted {"feujfie"} · {readTime} min
+          Posted {data && formatDateWithYear(new Date(data))} · {readTime} min
           read
         </p>
       </Box>
