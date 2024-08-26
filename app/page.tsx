db8f75f46eaf1f518a656734d88a978e242cde81
IvanCCO <ivanmedeiros0903@outlook.com>
2024-08-26 19:33:28 -0300

    Log: Test log

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,6 @@ import Home from "./Home";
 
 export default async function Page() {
   const postData = await getPostListData();
+  console.log("Teste");
   return <Home postsListData={postData} />;
 }
