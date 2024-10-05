9fade9da8790faf3708094e0189ee12a60bf8408
IvanCCO <ivanmedeiros0903@outlook.com>
2024-10-05 00:05:39 -0300

    Remove speed insights from project

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,26 +3,28 @@ import { Providers } from "./providers";
 import "./globals.css";
 import { Metadata } from "next";
 import { formatUrlDefault } from "./_lib/formatUrl";
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ivan Freire | meu Palácio Digital",
-  description: "Aqui eu compartilho meus pensamentos, ideias, opniões e muito mais.",
+  description:
+    "Aqui eu compartilho meus pensamentos, ideias, opniões e muito mais.",
   authors: { name: "Ivan Freire" },
   keywords: ["livro", "artigo", "codigo", "finanças", "HowTo", "tecnologia"],
   openGraph: {
     siteName: "Ivan Freire",
     title: "Ivan Freire | meu Palácio Digital",
-    description: "Aqui eu compartilho meus pensamentos, ideias, opniões e muito mais",
+    description:
+      "Aqui eu compartilho meus pensamentos, ideias, opniões e muito mais",
     images: formatUrlDefault("logo-taxco.png"),
     type: "article",
   },
   twitter: {
     title: "Ivan Freire | meu Palácio Digital",
-    description: "Aqui eu compartilho meus pensamentos, ideias, opniões e muito mais",
+    description:
+      "Aqui eu compartilho meus pensamentos, ideias, opniões e muito mais",
     images: formatUrlDefault("logo-taxco.png"),
   },
 };
@@ -36,7 +38,6 @@ export default function RootLayout({
     <html lang="en" style={{ colorScheme: "dark" }} suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>{children}</Providers>
-        <SpeedInsights/>
         <Analytics />
       </body>
     </html>
