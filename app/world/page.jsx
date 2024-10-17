e61d8e2da4f92407a2b37c8f442cc92af1e6e714
IvanCCO <ivanmedeiros0903@outlook.com>
2024-10-17 15:32:15 -0300

    Add World Metatag SEO

--- app/world/page.jsx
+++
@@ -1,6 +0,0 @@
-import World from "../article/[slug]/World";
-import { world } from "./worldData";
-
-export default function Page() {
-  return <World locations={world.features} />;
-}
---
+++ app/world/page.tsx
@@ -0,0 +1,38 @@
+import { formatUrlDefault } from "../_lib/formatUrl";
+import World from "../article/[slug]/World";
+import { world } from "./worldData";
+import type { Metadata } from "next";
+
+const openGraph = {
+  title: "Ivan Freire - Globo | Todos os lugares que já estive.",
+  description:
+    "Meu sonho é conhecer os 4 oceanos, ter contato com diversas culturas, aprender dezenas de idiomas, e não vou desistir.",
+};
+
+export const metadata: Metadata = {
+  title: openGraph.title,
+  description: openGraph.description,
+  openGraph: {
+    siteName: "Ivan Freire",
+    images: [
+      {
+        url: formatUrlDefault("world-thumb.png"),
+        width: 800,
+        height: 600,
+      },
+    ],
+    type: "website",
+    title: openGraph.title,
+    description: openGraph.description,
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: formatUrlDefault("world-thumb.png"),
+    title: openGraph.title,
+    description: openGraph.description,
+  },
+};
+
+export default function Page() {
+  return <World locations={world.features} />;
+}
