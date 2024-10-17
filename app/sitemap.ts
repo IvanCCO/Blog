d13aaa4d0fbfa417142645817e6f0db54df15d01
IvanCCO <ivanmedeiros0903@outlook.com>
2024-10-17 15:08:47 -0300

    SEO: Add robots and sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+import { MetadataRoute } from "next";
+
+const BASE_URL = "https://ivanfreire.me";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: BASE_URL,
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/about/me`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: `${BASE_URL}/world`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/about`,
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+  ];
+}
